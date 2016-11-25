96169f26a1869ad7c0b824dde9ba84da533d5b1d
xiaoF <870501@qq.com>
2016-11-25 11:07:28 +0800

    ip address

--- Docker/MongoConnector/mongoconnector.sh
+++ Docker/MongoConnector/mongoconnector.sh
@@ -2,4 +2,4 @@
 
 sleep 1
 
-mongo-connector --auto-commit-interval=0 -m 10.211.55.6:27017 -t 10.211.55.6:9200 -d elastic_doc_manager --namespace-set admin.comments,admin.events,admin.faq,admin.locations,admin.posts,admin.records,admin.schools,admin.tags,admin.users,admin.zipcodes --continue-on-error
+mongo-connector --auto-commit-interval=0 -m 192.168.3.168:27017 -t 192.168.3.168:9200 -d elastic_doc_manager --namespace-set admin.comments,admin.events,admin.faq,admin.locations,admin.posts,admin.records,admin.schools,admin.tags,admin.users,admin.zipcodes --continue-on-error
