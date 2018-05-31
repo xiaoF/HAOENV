f39252f18c44c8db421384591f37cf206569b171
小傅 <870501@qq.com>
2018-05-31 12:47:56 +0800

    mongo set

--- Docker/mongosetup/setup.sh
+++ Docker/mongosetup/setup.sh
@@ -21,15 +21,18 @@ mongo --host ${mongodb1}:${port} <<EOF
         "members": [
             {
                 "_id": 0,
-                "host": "${mongodb1}:${port}"
+                "host": "${mongodb1}:${port}",
+                "priority":2
             },
             {
                 "_id": 1,
-                "host": "${mongodb2}:${port}"
+                "host": "${mongodb2}:${port}",
+                "priority":1
             },
             {
                 "_id": 2,
-                "host": "${mongodb3}:${port}"
+                "host": "${mongodb3}:${port}",
+                "priority":1
             }
         ]
     };
